3aab2ac65f97080f52801c2dd5975a349efa6edd
daulet <daucloudlab@gmail.com>
2015-11-12 14:46:59 +0600

    Hibernate Test App by annotation

---
+++ src/main/java/kz/kazntu/lessons/common/App.java
@@ -0,0 +1,21 @@
+package kz.kazntu.lessons.common;
+
+import kz.kazntu.lessons.utils.HibernateUtil;
+import org.hibernate.Session;
+
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println("Maven + Hibernate + MySQL");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+        Stock stock = new Stock();
+
+        stock.setStockCode("4715");
+        stock.setStockName("GENM");
+
+        session.save(stock);
+        session.getTransaction().commit();
+    }
+}
